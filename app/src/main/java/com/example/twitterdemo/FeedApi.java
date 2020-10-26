3e4ffa76bf442bedfda6c405b89144987720aec3
shubham singh <shubhamsingh5955@gmail.com>
2020-10-26 15:16:51 +0530

    before login(listview complete)

--- app/src/main/java/com/example/twitterdemo/FeedApi.java
+++ app/src/main/java/com/example/twitterdemo/FeedApi.java
@@ -4,11 +4,16 @@ import com.example.twitterdemo.model.Feed;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface FeedApi {
 
     String BASE_URL="https://www.reddit.com/r/";
+    //static feed name
+    @GET("{feed_name}/.rss")
+    Call<Feed> getFeed(@Path("feed_name") String feed_name);
 
-    @GET("earthporn/.rss")
-    Call<Feed> getFeed();
+    //static feed name
+    //@GET("earthporn/.rss")
+    //Call<Feed> getFeed();
 }
--- app/src/main/java/com/example/twitterdemo/MainActivity.java
+++ app/src/main/java/com/example/twitterdemo/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.twitterdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.twitterdemo.Comments.CommentsActivity;
 import com.example.twitterdemo.model.Feed;
 import com.example.twitterdemo.model.entry.Entry;
 
@@ -18,81 +25,143 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG ="MainActivity";
-    private static final String BASE_URL="https://www.reddit.com/r/";
+    private static final String TAG = "MainActivity";
+
+    private static final String BASE_URL = "https://www.reddit.com/r/";
+
+    private Button btnRefreshFeed;
+    private EditText mFeedName;
+    private String currentFeed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting.");
+        btnRefreshFeed = (Button) findViewById(R.id.btnRefreshFeed);
+        mFeedName = (EditText) findViewById(R.id.etFeedName);
 
+        init();
+
+        btnRefreshFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String feedName = mFeedName.getText().toString();
+                if(!feedName.equals("")){
+                    currentFeed = feedName;
+                    init();
+                }
+                else{
+                    init();
+                }
+            }
+        });
+
+    }
+
+    private void init(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(SimpleXmlConverterFactory.create())
                 .build();
 
-        FeedApi feedApi = retrofit.create(FeedApi.class);
+        FeedApi feedAPI = retrofit.create(FeedApi.class);
 
-        Call<Feed> call=feedApi.getFeed();
+        Call<Feed> call = feedAPI.getFeed(currentFeed);
 
         call.enqueue(new Callback<Feed>() {
             @Override
-            public void onResponse(Call<Feed> call , Response<Feed> response) {
-                Log.e(TAG, "onResponce: feed " + response.body().toString() );
-                Log.e(TAG, "onResponce: Server Responce" + response.toString());
+            public void onResponse(Call<Feed> call, Response<Feed> response) {
+                //Log.d(TAG, "onResponse: feed: " + response.body().toString());
+                Log.d(TAG, "onResponse: Server Response: " + response.toString());
 
-                List<Entry> entrys= response.body().getEntrys();
-                Log.e(TAG,"onResponse: entrys: "+response.body().getEntrys());
+                List<Entry> entrys = response.body().getEntrys();
 
-            //    Log.e(TAG,"onResponse: author: "+entrys.get(0).getAuthor().getName());
-            //    Log.e(TAG,"onResponse: updated: "+entrys.get(0).getUpdated());
-            //    Log.e(TAG,"onResponse: title: "+entrys.get(0).getTitle());
+                Log.d(TAG, "onResponse: entrys: " + response.body().getEntrys());
 
-                ArrayList<Post> posts =new ArrayList<Post>();
-                for(int i = 0; i< entrys.size(); i++){
-                    ExtractXML extractXML1=new ExtractXML(entrys.get(i).getCotent(),"<a href=");
-                    List<String> postContent=extractXML1.start();
+//                Log.d(TAG, "onResponse: author: " + entrys.get(0).getAuthor().getName());
+//                Log.d(TAG, "onResponse: updated: " + entrys.get(0).getUpdated());
+//                Log.d(TAG, "onResponse: title: " + entrys.get(0).getTitle());
 
-                    ExtractXML extractXML2=new ExtractXML(entrys.get(i).getCotent(),"<img src=");
+                ArrayList<Post> posts = new ArrayList<Post>();
+                for (int i = 0; i< entrys.size(); i++){
+                    ExtractXML extractXML1 = new ExtractXML(entrys.get(i).getCotent(), "<a href=");
+                    List<String> postContent = extractXML1.start();
 
-                    try {
+                    ExtractXML extractXML2 = new ExtractXML(entrys.get(i).getCotent(), "<img src=");
+
+                    try{
                         postContent.add(extractXML2.start().get(0));
                     }catch (NullPointerException e){
                         postContent.add(null);
-                        Log.e(TAG,"onResponse: NullPointerException(thumbnail):"+e.getMessage());
+                        Log.e(TAG, "onResponse: NullPointerException(thumbnail):" + e.getMessage() );
                     }
                     catch (IndexOutOfBoundsException e){
-                         postContent.add(null);
-                        Log.e(TAG,"onResponse: IndexOutOfBoundsException(thumbnail):"+e.getMessage());
+                        postContent.add(null);
+                        Log.e(TAG, "onResponse: IndexOutOfBoundsException(thumbnail):" + e.getMessage() );
                     }
-                    int lastPosition = postContent.size() - 1;
-                    posts.add(new Post(
-                            entrys.get(i).getTitle(),
-                            entrys.get(i).getAuthor().getName(),
-                            entrys.get(i).getUpdated(),
-                            postContent.get(0),
-                            postContent.get(lastPosition)
 
-                    ));
+                    int lastPosition = postContent.size() - 1;
+                    try{
+                        posts.add(new Post(
+                                entrys.get(i).getTitle(),
+                                entrys.get(i).getAuthor().getName(),
+                                entrys.get(i).getUpdated(),
+                                postContent.get(0),
+                                postContent.get(lastPosition)
+                        ));
+                    }catch (NullPointerException e){
+                        posts.add(new Post(
+                                entrys.get(i).getTitle(),
+                                "None",
+                                entrys.get(i).getUpdated(),
+                                postContent.get(0),
+                                postContent.get(lastPosition)
+                        ));
+                        Log.e(TAG, "onResponse: NullPointerException: " + e.getMessage() );
+                    }
+
                 }
-                for(int j=0;j<posts.size();j++){
-                    Log.d(TAG,"onResponse:  \n " +
-                            "posURL: "+ posts.get(j).getpostURL() +"\n "
-                                    + "thumbnailURL: "+ posts.get(j).getThumbnailURl() +"\n " +
-                                    "Title: "+ posts.get(j).getTitle() +"\n " +
-                                    "Author: "+ posts.get(j).getAuthor() +"\n " +
-                                    "updated: "+ posts.get(j).getDate_updated() +"\n ");
+
+                for(int j = 0; j<posts.size(); j++){
+                    Log.d(TAG, "onResponse: \n " +
+                            "PostURL: " + posts.get(j).getpostURL() + "\n " +
+                            "ThumbnailURL: " + posts.get(j).getThumbnailURl() + "\n " +
+                            "Title: " + posts.get(j).getTitle() + "\n " +
+                            "Author: " + posts.get(j).getAuthor() + "\n " +
+                            "updated: " + posts.get(j).getDate_updated() + "\n ");
                 }
+
+                ListView listView = (ListView) findViewById(R.id.listView);
+                CustomListAdapter customListAdapter = new CustomListAdapter(MainActivity.this, R.layout.card_layout_main, posts);
+                listView.setAdapter(customListAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Log.d(TAG, "onItemClick: Clicked: " + posts.get(position).toString());
+                        Intent intent = new Intent(MainActivity.this, CommentsActivity.class);
+                        intent.putExtra("@string/post_url", posts.get(position).getpostURL());
+                        intent.putExtra("@string/post_thumbnail", posts.get(position).getThumbnailURl());
+                        intent.putExtra("@string/post_title", posts.get(position).getTitle());
+                        intent.putExtra("@string/post_author", posts.get(position).getAuthor());
+                        intent.putExtra("@string/post_updated", posts.get(position).getDate_updated());
+                        startActivity(intent);
+                    }
+                });
+
             }
+
             @Override
             public void onFailure(Call<Feed> call, Throwable t) {
-                Log.e(TAG, "onFailure: Unable to retrive RSS: "+ t.getMessage());
-                Toast.makeText(MainActivity.this,"An Error Occured",Toast.LENGTH_SHORT).show();
+                Log.e(TAG, "onFailure: Unable to retrieve RSS: " + t.getMessage() );
+                Toast.makeText(MainActivity.this, "An Error Occured", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
-
-
 }
